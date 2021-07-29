35c127e32dbf5026a9bba84897ede7d72af195f0
Maryanne Hooper <hooper.maryannem@gmail.com>
2021-07-29 03:00:05 -0400

    added css, animations and js form popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
+// Mobile Nav Bar Function
 function myFunction() {
   var x = document.getElementById("links");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+  }
+}
+
+
+// Popup Submit Form Confirmation
+// Get the modal
+var modal = document.getElementById("submitForm");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submitButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
